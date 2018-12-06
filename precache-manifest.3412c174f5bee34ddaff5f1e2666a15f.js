b6b0358f0649fc1b16532151cb766c25e752d68e
ZHamburglar <kollinbrandenburg@gmail.com>
2018-12-06 13:00:30 -0600

    Updates

---
+++ precache-manifest.3412c174f5bee34ddaff5f1e2666a15f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/austin-campaign-finance-expenditures/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a9a617c8502a3bfe9b33",
+    "url": "/austin-campaign-finance-expenditures/static/js/runtime~main.a9a617c8.js"
+  },
+  {
+    "revision": "bf286053142f384e31ab",
+    "url": "/austin-campaign-finance-expenditures/static/js/main.bf286053.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/austin-campaign-finance-expenditures/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "bf286053142f384e31ab",
+    "url": "/austin-campaign-finance-expenditures/static/css/main.57d26e94.chunk.css"
+  },
+  {
+    "revision": "2ec62a836163109cca77a67f232dbd49",
+    "url": "/austin-campaign-finance-expenditures/index.html"
+  }
+];
