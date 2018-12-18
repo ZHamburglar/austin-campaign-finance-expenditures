b87287e07615dae5befaca7f4aad6b7349ceabd8
ZHamburglar <kollinbrandenburg@gmail.com>
2018-12-18 13:55:40 -0600

    Updates

---
+++ precache-manifest.9c9edc1797faacfebdca1ecbccf660a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "59b4367729232b37949b51d99326b3d4",
+    "url": "/austin-campaign-finance-expenditures/static/media/coadowntown.59b43677.jpg"
+  },
+  {
+    "revision": "a9a617c8502a3bfe9b33",
+    "url": "/austin-campaign-finance-expenditures/static/js/runtime~main.a9a617c8.js"
+  },
+  {
+    "revision": "738e1c7097f33d115282",
+    "url": "/austin-campaign-finance-expenditures/static/js/main.738e1c70.chunk.js"
+  },
+  {
+    "revision": "aa00a6fe99c475ffa2bc",
+    "url": "/austin-campaign-finance-expenditures/static/js/1.aa00a6fe.chunk.js"
+  },
+  {
+    "revision": "738e1c7097f33d115282",
+    "url": "/austin-campaign-finance-expenditures/static/css/main.cdf70e6c.chunk.css"
+  },
+  {
+    "revision": "aa00a6fe99c475ffa2bc",
+    "url": "/austin-campaign-finance-expenditures/static/css/1.93afb396.chunk.css"
+  },
+  {
+    "revision": "8677ccdc1daca5a2a3cedec06288044d",
+    "url": "/austin-campaign-finance-expenditures/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/austin-campaign-finance-expenditures/precache-manifest.96327150091b5254d2a4f1605956bf3a.js"
+  "/austin-campaign-finance-expenditures/precache-manifest.9c9edc1797faacfebdca1ecbccf660a6.js"
 );
 
 workbox.clientsClaim();
